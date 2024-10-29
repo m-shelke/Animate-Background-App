7dd1f6d060288e18f14e433a66f49ef2d8366adb
AtomicFive4059 <mshelke074@gmail.com>
2024-10-29 22:05:15 +0530

    Animated Background

---
+++ app/src/main/java/com/example/animatebackgroundapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.animatebackgroundapp;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+    ConstraintLayout constraintLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        constraintLayout = findViewById(R.id.main);
+
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+}
